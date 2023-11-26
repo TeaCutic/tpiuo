cbd1454f5ea01a665e4f036386f586e98540e468
tea <tea.cutic@fer.hr>
2023-11-26 18:43:36 +0100

    lab2 zad2

--- lab1/consumer/consumer.py
+++ lab1/consumer/consumer.py
@@ -1,18 +1,34 @@
 import asyncio
 from azure.eventhub.aio import EventHubConsumerClient
+from azure.storage.filedatalake import DataLakeServiceClient
+import json
+import datetime
 
 connection_str = "Endpoint=sb://tpiuolab1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=1j4xz6Ea7PDfr5kBbxfjSsLPKN+TIu+uq+AEhGgH5oA="
 consumer_group = "$default"
 eventhub_name = "lab1"
 client = EventHubConsumerClient.from_connection_string(connection_str, consumer_group, eventhub_name=eventhub_name)
 
+container_name="sl-container"
+connection_string_sl = "DefaultEndpointsProtocol=https;AccountName=tpioustoragelake;AccountKey=83qMNtwGo0uaZ0NYoudmlna7DziWb5nJp4w0f9DYzV00XiI3MCLHTfP85ZAboeSzNQUe9ay6kFuF+AStwISi3w==;EndpointSuffix=core.windows.net"
+service_client = DataLakeServiceClient.from_connection_string(connection_string_sl)
 
 async def on_event(partition_context, event):
-    print(
-        'Message data: {}'.format(
-            event.body_as_str(encoding="UTF-8")
-        )
-    )
+    print("Got data")
+    data = json.loads(event.body_as_str(encoding="UTF-8"))
+    date = data["created_utc"]
+    directory_name = datetime.datetime.utcfromtimestamp(date).strftime('%Y/%m/%d/%H/%M')
+
+    file_system_client = service_client.get_file_system_client(file_system=container_name)
+    dir_client = file_system_client.get_directory_client(directory_name)
+
+    if not dir_client.exists():
+        dir_client.create_directory()
+    file_name = str(data["id"]) + "_" + str(data["title"])
+    file_client = dir_client.create_file(file_name + ".txt")
+    file_client.append_data(data, 0, len(data))
+    file_client.flush_data(len(data))
+
     await partition_context.update_checkpoint(event)
 
 async def main():
--- lab1/producer/producer.py
+++ lab1/producer/producer.py
@@ -1,6 +1,7 @@
 import requests
 import asyncio
 import json
+import time
 
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
@@ -13,31 +14,42 @@ EVENT_HUB_CONNECTION_STR = "Endpoint=sb://tpiuolab1.servicebus.windows.net/;Shar
 EVENT_HUB_NAME = "lab1"
 
 async def sendData(r):
-    async with producer:
+     async with producer:
         event_data_batch = await producer.create_batch()
 
-        for i in range (10):
+        for i in range (limit):
          data = r["data"]["children"][i]["data"]
          data = json.dumps(data, indent = 4)
          event_data_batch.add(EventData(data))
 
-        await producer.send_batch(event_data_batch) 
-        while True:
-         pass    
+        await producer.send_batch(event_data_batch)      
   
  
-def get_reddit(subreddit,listing,limit,timeframe):
+def get_reddit(subreddit,listing,limit,timeframe,after_flag):
     try:
-        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
+        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}&after={after_flag}'
         request = requests.get(base_url, headers = {'User-agent': 'yourbot'})
     except:
         print('An Error Occured')
     return request.json()
  
 
-r = get_reddit(subreddit,listing,limit,timeframe)
-
 producer = EventHubProducerClient.from_connection_string(
         conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
     )
-asyncio.run(sendData(r)) 
+
+after = 1
+after_flag=None
+
+while after == 1:
+   r = get_reddit(subreddit,listing,limit,timeframe,after_flag)
+   asyncio.run(sendData(r))
+   after_flag = r['data']['after']
+   if after_flag == None:
+      after = 0
+   else:
+      time.sleep(10)
+    
+while 1==1:
+   pass
+
