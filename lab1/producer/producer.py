c6736a123e3d88c351ee56fadfdd89fbb97582d3
tea <tea.cutic@fer.hr>
2023-12-02 17:29:14 +0100

    10 obajava

--- lab1/producer/producer.py
+++ lab1/producer/producer.py
@@ -1,11 +1,10 @@
 import requests
 import asyncio
 import json
-import time
 
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
- 
+
 subreddit = 'dataengineering'
 limit = 10
 timeframe = 'all'
@@ -14,43 +13,31 @@ EVENT_HUB_CONNECTION_STR = "Endpoint=sb://tpiuolab1.servicebus.windows.net/;Shar
 EVENT_HUB_NAME = "lab1"
 
 async def sendData(r):
-     async with producer:
+    async with producer:
         event_data_batch = await producer.create_batch()
 
-        for i in range (limit):
-            data = r["data"]["children"][i]["data"]
-            data = json.dumps(data, indent = 4)
-            event_data_batch.add(EventData(data)) 
+        for i in range (10):
+         data = r["data"]["children"][i]["data"]
+         data = json.dumps(data, indent = 4)
+         event_data_batch.add(EventData(data))
 
-        await producer.send_batch(event_data_batch)    
-        print("Data sent")  
-  
- 
-def get_reddit(subreddit,listing,limit,timeframe,after_flag):
+        await producer.send_batch(event_data_batch) 
+        while True:
+         pass    
+
+
+def get_reddit(subreddit,listing,limit,timeframe):
     try:
-        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}&after={after_flag}'
+        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
         request = requests.get(base_url, headers = {'User-agent': 'yourbot'})
     except:
         print('An Error Occured')
     return request.json()
- 
+
+
+r = get_reddit(subreddit,listing,limit,timeframe)
 
 producer = EventHubProducerClient.from_connection_string(
         conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
     )
-
-after = 1
-after_flag=None
-
-while after == 1:
-   r = get_reddit(subreddit,listing,limit,timeframe,after_flag)
-   asyncio.run(sendData(r))
-   after_flag = r['data']['after']
-   if after_flag == None:
-      after = 0
-   else:
-      time.sleep(10)
-    
-while 1==1:
-   pass
-
+asyncio.run(sendData(r))
